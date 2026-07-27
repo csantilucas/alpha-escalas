067614a72f4e8ccf38dcb76d4bf94334a185e6e1
lucas santi <lucascsanti2006@gmail.com>
2026-07-27 08:58:23 -0400

    fix: ensure PORT is a number by explicitly converting from environment variable

--- backend-main/index.ts
+++ backend-main/index.ts
@@ -1,6 +1,6 @@
 import app, { bootstrap } from "./src/config/server.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT || '3000');
 const HOST = process.env.HOST ?? "0.0.0.0";
 
 app.listen(PORT, HOST, async () => {
